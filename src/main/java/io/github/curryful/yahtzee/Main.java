e6f8cb0078972dace764c73804690b69cf330157
Nico Lerchl <nico@lerchl.one>
2024-05-06 17:51:07 +0200

    framework changes

--- src/main/java/io/github/curryful/yahtzee/Main.java
+++ src/main/java/io/github/curryful/yahtzee/Main.java
@@ -10,28 +10,29 @@ import io.github.curryful.rest.middleware.PreMiddleware;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // Create a list of pre-middleware
-        MutableArrayList<PreMiddleware> preMiddleware = MutableArrayList.empty();
-        preMiddleware.add(new SessionValidationMiddleware()); // Adds session token validation
+	public static void main(String[] args) {
+		// Create a list of pre-middleware
+		MutableArrayList<PreMiddleware> preMiddleware = MutableArrayList.empty();
+		preMiddleware.add(new SessionValidationMiddleware()); // Adds session token validation
 
-        // Create a list of post-middleware
-        MutableArrayList<PostMiddleware> postMiddleware = MutableArrayList.empty();
-        postMiddleware.add(new CustomHeaderPostMiddleware()); // Adds custom headers for Bob
+		// Create a list of post-middleware
+		MutableArrayList<PostMiddleware> postMiddleware = MutableArrayList.empty();
+		postMiddleware.add(new CustomHeaderPostMiddleware()); // Adds custom headers for Bob
 
-        // Setup the endpoints
-        MutableArrayList<Endpoint> endpoints = MutableArrayList.empty();
-        endpoints.add(Endpoint.of(Destination.of(HttpMethod.POST, "/login"), YahtzeeEndpoints.login));
-        endpoints.add(Endpoint.of(Destination.of(HttpMethod.GET, "/yahtzee"), YahtzeeEndpoints.playYahtzee));
+		// Setup the endpoints
+		MutableArrayList<Endpoint> endpoints = MutableArrayList.empty();
+		endpoints.add(Endpoint.of(Destination.of(HttpMethod.POST, "/login"), YahtzeeEndpoints.login));
+		endpoints.add(Endpoint.of(Destination.of(HttpMethod.GET, "/yahtzee"), YahtzeeEndpoints.playYahtzee));
 
-        // Listening on port 8080
-        var listenTry = Server.listen.apply(preMiddleware) // Apply pre-middleware
-                .apply(endpoints) // Set endpoints
-                .apply(postMiddleware) // Apply post-middleware
-                .apply(8080); // Set the port
+		// Listening on port 8080
+		var listenTry = Server.listen.apply(System.out::println)
+				.apply(preMiddleware) // Apply pre-middleware
+				.apply(endpoints) // Set endpoints
+				.apply(postMiddleware) // Apply post-middleware
+				.apply(8080); // Set the port
 
-        if (listenTry.isFailure()) {
-            listenTry.getError().printStackTrace();
-        }
-    }
+		if (listenTry.isFailure()) {
+			listenTry.getError().printStackTrace();
+		}
+	}
 }
